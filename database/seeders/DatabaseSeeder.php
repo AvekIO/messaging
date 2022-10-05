c459ccdb91380e619f58609e6d57048b528700ec
“Avek.io <tech@avek.io>
2022-10-05 09:05:08 +0300

    Add seeding for telegram_user model

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -11,5 +11,6 @@ class DatabaseSeeder extends Seeder
     {
         $this->call(ChatSeeder::class);
         $this->call(MessageSeeder::class);
+        $this->call(TelegramUserSeeder::class);
     }
 }
